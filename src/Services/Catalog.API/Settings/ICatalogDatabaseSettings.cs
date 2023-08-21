ff78ae36bb797d736ae0187e61b8b84294021187
phong.bui <buivan.phong94@gmail.com>
2023-08-21 22:29:52 +0700

    Rename project and add catalog context

---
+++ src/Services/Catalog.API/Settings/ICatalogDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Settings;
+
+public interface ICatalogDatabaseSettings
+{
+    string ConnectionString { get; set; }
+    string DatabaseName { get; set; }
+    string CollectionName { get; set; }
+}
